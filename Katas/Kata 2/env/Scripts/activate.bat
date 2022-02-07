8eec2cae854a6d36dbdf1f8f8e0aa05ed1976f94
Tomás García García <toms_gr@hotmail.com>
2022-02-07 12:39:29 -0600

    Kata 1 y 2

--- Katas/Kata 2/env/Scripts/activate.bat
+++ Katas/Kata 2/env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Launch X\Modulo 2\Kata\env
+set VIRTUAL_ENV=D:\Launch X\Innovaccion\Katas\Kata 2\env
 
 if not defined PROMPT set PROMPT=$P$G
 
